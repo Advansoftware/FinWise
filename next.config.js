efa67ec07c26931f0e6e9c2eaa00ab7728cd3422
Bruno Antunes <brunoantunes100@gmail.com>
2025-09-02 21:32:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+        ],
+    },
     transpilePackages: ['tr46', 'whatwg-url'],
     experimental: {
         serverComponentsExternalPackages: [
